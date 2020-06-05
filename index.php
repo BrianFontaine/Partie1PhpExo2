4c957e612c42f82eff940acf8651bbe78e9a8c12
Brian Fontaine <brian02440@live.fr>
2020-06-05 12:12:40 +0200

    exo2

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    include'../header.php';
+    $lastName = 'Fontaine';
+    $firstName = 'Brian';
+    $age = 22;
+?>
+    <h1>Exercices 2</h1>
+    <p><?="Bonjour je m'appelle ".$lastName." ".$firstName." ".$age."<br>";?></p>
+</body>
+<?php
+    include'../footer.php';
+?>
